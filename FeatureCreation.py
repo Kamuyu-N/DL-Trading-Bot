3bd5e7786a6a1d27bff804686600f98b91c2cee1
Kamuyu-N <samuelnjugunamuyu@gmail.com>
2025-03-19 02:48:06 +0300

    Replaced function used to calculate features. Also, began using log OHLC data as seperate features ( model to have two inputs - OHLC and technical indicators

--- FeatureCreation.py
+++ FeatureCreation.py
@@ -7,72 +7,76 @@ from sklearn.preprocessing import MinMaxScaler, LabelEncoder
 pd.options.display.width = 0
 
 #import the data
-# price = pd.read_csv('C:/Users/muyu2/OneDrive/Documents/DeepLearning/bbands-test.csv', sep='\t')
 price = pd.read_csv('C:/Users/muyu2/OneDrive/Documents/DeepLearning/eurusd-15m.csv', sep=';')
 
 price.columns = ['date', 'time', 'open', 'high','low','close', 'volume']
-# price.columns = ['date', 'time', 'open', 'high','low','close','tik_vol', 'volume', 'spreead']
 price['datetime'] = price['date'] + " "+ price['time']
-
-# price['datetime'] = pd.to_datetime(price["datetime"], dayfirst=True)
 price.drop(['date', 'time'], axis=1, inplace=True)
 
 #Change column arrangement
 price = price[['datetime', 'open','high','low','close', 'volume']]
-
 price.drop('volume', axis= 1, inplace=True)
 
 #Create 30 min timeframe data
 technicals_price = price.copy()
 technicals = price.copy()
 
-# Label creation
-x = ta.bbands(price.close, std=2, length=20, mamode='ema')
-adx = ta.adx(technicals['high'], technicals['low'], technicals['close'])
+#Create log prices
+cols = ['open', 'high', 'low', 'close']
+for col in cols:
+    technicals[f'log_{col}'] = np.log(technicals[col] / technicals[col].shift(1))
 
-technicals = pd.concat([technicals, x, adx], axis=1)
-
-
-#Addition of other indicators
+#Feature creation
 technicals['ema_mid'] = ta.ema(technicals['close'], length=100)
-technicals['ema_long'] = ta.ema(technicals['close'], length=100)
-technicals['ema_200'] = ta.ema(technicals['close'], length=200)
+technicals['ema_long'] = ta.ema(technicals['close'], length=200)
+technicals['ema_short'] = ta.ema(technicals['close'], length=50)
+technicals['atr'] = ta.atr(technicals['high'], technicals['low'], technicals['close'], length=14)
 
 technicals.dropna(axis = 0, inplace = True)
 
-
+#Calculate the distance between price and current moving average
+technicals['ema_short'] = (technicals['ema_short'] - technicals['close'])
 technicals['ema_mid'] = (technicals['ema_mid'] - technicals['close'])
 technicals['ema_long']= (technicals['ema_long'] - technicals['close'])
 
-
+#Splitting the data to training and test sets
 technicals_train = technicals[:round(len(technicals) * 0.80)].copy()
 technicals_test  = technicals[round(len(technicals_price) * 0.80):].copy()
 
+
+#Scaling the features
 scaler = MinMaxScaler(feature_range=(-1,1))
-technicals_train[['ema_mid','ema_long','DMP_14', 'DMN_14', 'ADX_14']] = scaler.fit_transform(technicals_train[['ema_mid','ema_long','DMP_14', 'DMN_14', 'ADX_14']])
+technicals_train[['ema_mid','ema_long','ema_short','log_open', 'log_high', 'log_low', 'log_close','atr']] = scaler.fit_transform(technicals_train[['ema_mid','ema_long','ema_short','log_open', 'log_high', 'log_low', 'log_close','atr']])
 
-technicals_test[['ema_mid','ema_long','DMP_14', 'DMN_14', 'ADX_14']] = scaler.transform(technicals_test[['ema_mid','ema_long','DMP_14', 'DMN_14', 'ADX_14']])
+technicals_test[['ema_mid','ema_long','ema_short','log_open', 'log_high', 'log_low', 'log_close','atr']] = scaler.transform(technicals_test[['ema_mid','ema_long','ema_short','log_open', 'log_high', 'log_low', 'log_close','atr']])
 
-technicals_test.reset_index(inplace = True, drop = True)
-technicals_train.reset_index(inplace = True ,drop = True)
+#Creation of input 1 features ( OHLC )
+log_prices_test = technicals_test[['log_open', 'log_high', 'log_low', 'log_close']].values.flatten().reshape(len(technicals_test), 4)# 4 represents the OHLC features
+log_prices_train = technicals_train[['log_open', 'log_high', 'log_low', 'log_close']].values.flatten().reshape(len(technicals_train), 4)
 
 print("Technicals calculation complete\n\nTime Step calculation beginning now...")
 
 kwargs = {
-    'forward_length': 16,
-    'timestep_length' : 50,
-   'indicator_columns': ['ema_long', 'DMP_14', 'DMN_14'],
-    'repeat' : False
+    'sl': 0.0010, #Desired stop loss ( from entry position)
+    'tp':0.0015, #Desired take-profit (from the entry position)
+    'look_forward':15, #max length to check if the sl/ tp level has been passed
+    'look_back':30, #timestep length desired
+    'columns_wanted':['ema_mid', 'ema_long','ema_short','atr']
 }
 
-#label creation
-train_label , train_indicators = DeepLearning.price_gen_V3(technicals_train, **kwargs)
+#label and feature creation
+train_label , train_indicators = DeepLearning.label_creator(technicals_train, **kwargs)
+test_label , test_indicators = DeepLearning.label_creator(technicals_test, **kwargs)
 
-test_label , test_indicators = DeepLearning.price_gen_V3(technicals_test, **kwargs)
+#Obsv distribution of the classes in the train set ( use to determine if class weights should be modified )
+print(pd.Series(train_label).value_counts())
 
-print(pd.Series(train_label).value_counts())#Obsv distribution of the classes in the train set
 
-#Save data
+#Save the Features input 1
+np.save(f'C:/Users/muyu2/OneDrive/Documents/DeepLearning/new/test/ohlc_test.npy', log_prices_test)
+np.save(f'C:/Users/muyu2/OneDrive/Documents/DeepLearning/new/train/ohlc_train.npy', log_prices_train)
+
+#Save the Features input 2
 np.save(f'C:/Users/muyu2/OneDrive/Documents/DeepLearning/new/test/techs.npy', np.array(test_indicators))
 np.save(f'C:/Users/muyu2/OneDrive/Documents/DeepLearning/new/train/techs.npy', np.array(train_indicators))
 
